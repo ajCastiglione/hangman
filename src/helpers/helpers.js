aefb7a2723a9af84465b0502a5e2cf717167afc4
Antonio Castiglione <antonio@minervawebdevelopment.com>
2021-05-22 14:15:04 -0400

    updated to use secure url

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -25,7 +25,7 @@ export function checkWin(correct, wrong, word) {
 
 export async function RandomWord() {
     const apiUrl =
-        "http://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
+        "https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
 
     return fetch(apiUrl)
         .then((res) => res.json())
