9e1dfefec2bb1520b39a2c1e573cf6b255a02a64
Antonio Castiglione <antonio@minervawebdevelopment.com>
2021-05-23 10:02:22 -0400

    updated to use more common words, added small disclaimer that word may include dashes

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ const Header = () => {
         <>
             <h1>Hangman</h1>
             <p>Find the hidden word - Enter a letter</p>
+            <small>** Dashes are the only allowed special character.</small>
         </>
     );
 };
